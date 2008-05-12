549f339b9903ca59f2159307ed3bd5cb0f11c81b
Tony Garnock-Jones <tonyg@lshift.net>
2008-05-12 17:19:50 +0100

    More test cases.

--- graph.js
+++ graph.js
@@ -52,9 +52,44 @@ Graph = {
 	    Graph.Tests._add(g, 'c', ['a']);
 	    Graph.Tests._add(g, 'd', ['b', 'c']);
 	    Graph.Tests._add(g, 'e', ['b', 'c']);
+	    print("** t1");
 	    print(Graph.least_common_ancestor(g, 'd', 'e'));
+	    print(Graph.least_common_ancestor(g, 'e', 'd'));
+	},
+
+	t2: function () {
+	    var g = {};
+	    Graph.Tests._add(g, 'a', []);
+	    Graph.Tests._add(g, 'b', ['a']);
+	    Graph.Tests._add(g, 'c', ['b']);
+	    Graph.Tests._add(g, 'd', ['c', 'g']);
+	    Graph.Tests._add(g, 'e', ['d', 'h']);
+	    Graph.Tests._add(g, 'f', ['e']);
+	    Graph.Tests._add(g, 'g', ['b']);
+	    Graph.Tests._add(g, 'h', ['g']);
+	    Graph.Tests._add(g, 'i', ['c', 'h']);
+	    Graph.Tests._add(g, 'j', ['i']);
+	    print("** t2");
+	    print(Graph.least_common_ancestor(g, 'f', 'j'));
+	    print(Graph.least_common_ancestor(g, 'j', 'f'));
+	},
+
+	t3: function () {
+	    var g = {};
+	    Graph.Tests._add(g, 'a', []);
+	    Graph.Tests._add(g, 'b', ['a']);
+	    Graph.Tests._add(g, 'c', []);
+	    Graph.Tests._add(g, 'd', ['c']);
+	    print("** t3");
+	    print(Graph.least_common_ancestor(g, 'b', 'd'));
+	},
+
+	tests: function () {
+	    Graph.Tests.t1();
+	    Graph.Tests.t2();
+	    Graph.Tests.t3();
 	}
     }
 };
 
-Graph.Tests.t1();
+Graph.Tests.tests();
