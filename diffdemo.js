3da6d523aa5d573f5cc7dd16d5a99024d2e0a9e2
Tony Garnock-Jones <tonyg@lshift.net>
2008-05-28 06:30:34 +0100

    Diff3 demo page and code.

--- diffdemo.js
+++ diffdemo.js
@@ -9,6 +9,7 @@ function make_getter_setter(id, should_splitjoin) {
     }
 }
 
+var f0 = make_getter_setter("f0", true);
 var f1 = make_getter_setter("f1", true);
 var f2 = make_getter_setter("f2", true);
 var f3 = make_getter_setter("f3", false);
@@ -33,4 +34,32 @@ function demo_invert() {
 
 function demo_apply_patch() {
     f2(Diff.patch(f1(), eval(f3())));
+}
+
+function demo_diff1() {
+    f3(uneval(Diff.diff_patch(f0(), f1())));
+}
+
+function demo_diff2() {
+    f3(uneval(Diff.diff_patch(f0(), f2())));
+}
+
+function demo_diff3(excludeFalseConflicts) {
+    f3(uneval(Diff.diff3_merge(f1(), f0(), f2(), excludeFalseConflicts)));
+}
+
+function demo_diff3_cooked(excludeFalseConflicts) {
+    var merger = Diff.diff3_merge(f1(), f0(), f2(), excludeFalseConflicts);
+    var lines = [];
+    for (var i = 0; i < merger.length; i++) {
+	var item = merger[i];
+	if (item.ok) {
+	    lines = lines.concat(item.ok);
+	} else {
+	    lines = lines.concat(["\n<<<<<<<<<\n"], item.conflict.a,
+				 ["\n=========\n"], item.conflict.b,
+				 ["\n>>>>>>>>>\n"]);
+	}
+    }
+    f3(lines.join(" "));
 }
