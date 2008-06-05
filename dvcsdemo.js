28b38a19b70ae3d79032c81356450a8a87c2cb86
Tony Garnock-Jones <tonyg@lshift.net>
2008-06-05 11:40:09 +0100

    Add presets; also include ambiguousLCA preset

--- dvcsdemo.js
+++ dvcsdemo.js
@@ -155,6 +155,26 @@ function newfile() {
 }
 
 function dvcsdemo_main() {
+    selectPresetNamed("clear");
+}
+
+function dumpRepo() {
+    log(JSON.stringify(repo, null, 2));
+}
+
+function toggleLogVisible() {
+    var e = document.getElementById("log");
+    e.className = (e.className == "invisible") ? "" : "invisible";
+}
+
+var presets = {};
+presets.clear = function () {
+    repo = new Dvcs.Repository();
+    fs = repo.update(null);
+}
+
+presets.preset1 = function () {
+    presets.clear();
     var fileA = fs.createFile();
     fs.setProp(fileA, "name", "File A");
     fs.setProp(fileA, "text", "A B C D E".split(/ /));
@@ -168,12 +188,45 @@ function dvcsdemo_main() {
     fs.setProp(fileA, "name", "File A, renamed");
     fs.setProp(fileA, "text", "A B X D E".split(/ /));
     var rC = repo.commit(fs);
-/*
+}
+
+presets.preset2 = function () {
+    presets.clear();
+    var fileA = fs.createFile();
+    fs.setProp(fileA, "name", "File A");
+    fs.setProp(fileA, "text", "A B C D E".split(/ /));
+    var rA = repo.commit(fs);
+    fs.setBranch("BBB");
+    fs.setProp(fileA, "text", "G G G A B C D E".split(/ /));
+    var rB1 = repo.commit(fs);
+    fs.setProp(fileA, "text", "A B C D E G G G A B C D E".split(/ /));
+    var rB2 = repo.commit(fs);
+    fs = repo.update(rA);
+    fs.setProp(fileA, "name", "File A, renamed");
+    fs.setProp(fileA, "text", "A B X D E".split(/ /));
+    var rC = repo.commit(fs);
+
     fs = repo.update(rB2);
     fs.setProp(fileA, "text", "A B Z D E G G G A B C D E".split(/ /));
     var rB3 = repo.commit(fs);
+}
 
-// -------
+presets.preset3 = function () {
+    presets.clear();
+    var fileA = fs.createFile();
+    fs.setProp(fileA, "name", "File A");
+    fs.setProp(fileA, "text", "A B C D E".split(/ /));
+    var rA = repo.commit(fs);
+    fs.setBranch("BBB");
+    fs.setProp(fileA, "text", "G G G A B C D E".split(/ /));
+    var rB1 = repo.commit(fs);
+    fs.setProp(fileA, "text", "A B C D E G G G A B C D E".split(/ /));
+    var rB2 = repo.commit(fs);
+    fs = repo.update(rA);
+    fs.setProp(fileA, "name", "File A, renamed");
+    fs.setProp(fileA, "text", "A B X D E".split(/ /));
+    var rC = repo.commit(fs);
+
     var mergeResult = repo.merge(rC, rB2);
     var rMerger = repo.commit(mergeResult.files);
     fs = repo.update(rMerger);
@@ -182,18 +235,172 @@ function dvcsdemo_main() {
     var rB3 = repo.commit(fs);
     var rMerger2 = repo.commit(repo.merge(rB3, rMerger).files);
     fs = repo.update(rMerger2);
-*/
+}
+
+presets.ambiguousLCA = function () {
+    var repoExt = {
+	"bodies": {
+	    "46c316ca-5c4f-4474-a7ea-cfcea3b2ca0f": {
+		"name": "The File",
+		"text": ["a"]
+	    },
+	    "8e184218-c4d8-426b-b190-916e47249ec6": {
+		"name": "The File",
+		"text": ["b"]
+	    },
+	    "2f1dd5b6-271b-4adc-a3ea-cbd11588e95d": {
+		"name": "The File",
+		"text": ["c"]
+	    },
+	    "6a28a74c-727b-4ea0-bda0-aa88c53c730c": {
+		"name": "The File",
+		"text": ["g"]
+	    },
+	    "50a9df9e-e1c9-4138-a7d4-042d6b0e1e1d": {
+		"name": "The File",
+		"text": ["d"]
+	    },
+	    "98a26661-3cc4-42bc-9cac-1d01dd64b7f3": {
+		"name": "The File",
+		"text": ["h"]
+	    },
+	    "c8a089a0-abf3-4940-935f-6a039fdb32ed": {
+		"name": "The File",
+		"text": ["i"]
+	    },
+	    "3e805399-d30b-4cd8-8e29-52d98650de77": {
+		"name": "The File",
+		"text": ["e"]
+	    }
+	},
+	"revisions": {
+	    "814f45c3-916d-4a51-8f91-f8c8ed24c598": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "46c316ca-5c4f-4474-a7ea-cfcea3b2ca0f"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662063125,
+		"directParent": null,
+		"additionalParent": null
+	    },
+	    "ff9b6da5-b759-4c8c-8c5d-8497504f4529": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "8e184218-c4d8-426b-b190-916e47249ec6"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662070767,
+		"directParent": "814f45c3-916d-4a51-8f91-f8c8ed24c598",
+		"additionalParent": null
+	    },
+	    "d092f83a-5e91-42aa-bd6e-d86e8aaeeffd": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "2f1dd5b6-271b-4adc-a3ea-cbd11588e95d"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662073640,
+		"directParent": "ff9b6da5-b759-4c8c-8c5d-8497504f4529",
+		"additionalParent": null
+	    },
+	    "737f2636-9db0-4369-8521-a9b9d64d8fc6": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "6a28a74c-727b-4ea0-bda0-aa88c53c730c"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662085012,
+		"directParent": "814f45c3-916d-4a51-8f91-f8c8ed24c598",
+		"additionalParent": null
+	    },
+	    "f5a7d71d-41d0-4d69-8434-c0fb68ba1bde": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "50a9df9e-e1c9-4138-a7d4-042d6b0e1e1d"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662096545,
+		"directParent": "737f2636-9db0-4369-8521-a9b9d64d8fc6",
+		"additionalParent": "d092f83a-5e91-42aa-bd6e-d86e8aaeeffd"
+	    },
+	    "f06bc572-c234-4a71-980f-4a9d58bea46c": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "98a26661-3cc4-42bc-9cac-1d01dd64b7f3"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662104627,
+		"directParent": "737f2636-9db0-4369-8521-a9b9d64d8fc6",
+		"additionalParent": null
+	    },
+	    "33b9c253-9378-4115-9cad-dfa0c81efc57": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "c8a089a0-abf3-4940-935f-6a039fdb32ed"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662119727,
+		"directParent": "f06bc572-c234-4a71-980f-4a9d58bea46c",
+		"additionalParent": "d092f83a-5e91-42aa-bd6e-d86e8aaeeffd"
+	    },
+	    "554a6354-f6d5-4033-9fc5-b5b7a4cf5991": {
+		"alive": {
+		    "d077723c-5169-45dc-a465-0668870979d6": "3e805399-d30b-4cd8-8e29-52d98650de77"
+		},
+		"dead": {},
+		"changed": ["d077723c-5169-45dc-a465-0668870979d6"],
+		"branch": null,
+		"timestamp": 1212662141759,
+		"directParent": "f5a7d71d-41d0-4d69-8434-c0fb68ba1bde",
+		"additionalParent": "f06bc572-c234-4a71-980f-4a9d58bea46c"
+	    }
+	},
+	"children": {
+	    "814f45c3-916d-4a51-8f91-f8c8ed24c598": [
+		"ff9b6da5-b759-4c8c-8c5d-8497504f4529",
+		"737f2636-9db0-4369-8521-a9b9d64d8fc6"
+	    ],
+	    "ff9b6da5-b759-4c8c-8c5d-8497504f4529": [
+		"d092f83a-5e91-42aa-bd6e-d86e8aaeeffd"
+	    ],
+	    "737f2636-9db0-4369-8521-a9b9d64d8fc6": [
+		"f5a7d71d-41d0-4d69-8434-c0fb68ba1bde",
+		"f06bc572-c234-4a71-980f-4a9d58bea46c"
+	    ],
+	    "d092f83a-5e91-42aa-bd6e-d86e8aaeeffd": [
+		"f5a7d71d-41d0-4d69-8434-c0fb68ba1bde",
+		"33b9c253-9378-4115-9cad-dfa0c81efc57"
+	    ],
+	    "f06bc572-c234-4a71-980f-4a9d58bea46c": [
+		"33b9c253-9378-4115-9cad-dfa0c81efc57",
+		"554a6354-f6d5-4033-9fc5-b5b7a4cf5991"
+	    ],
+	    "f5a7d71d-41d0-4d69-8434-c0fb68ba1bde": [
+		"554a6354-f6d5-4033-9fc5-b5b7a4cf5991"
+	    ]
+	}
+    };
+
+    presets.clear();
+    repo.importRevisions(repoExt);
+}
+
+function selectPreset() {
+    selectPresetNamed(document.getElementById("presetSelect").value);
+}
+
+function selectPresetNamed(name) {
+    presets[name]();
     redisplay_repository_history();
     sync_view_of_fs();
-}
-
-function dumpRepo() {
-    log(JSON.stringify(repo, null, 2));
-}
-
-function toggleLogVisible() {
-    var e = document.getElementById("log");
-    e.className = (e.className == "invisible") ? "" : "invisible";
 }
 
 function log() {
