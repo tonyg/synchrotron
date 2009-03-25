30ee17e498281f4635f8db8773d639149a4d4275
Tony Garnock-Jones <tonyg@lshift.net>
2009-03-25 12:42:11 +0000

    Factor out repo drawing

--- mctest.js
+++ mctest.js
@@ -18,6 +18,60 @@ noisyLoad("draw.js");
 noisyLoad("sha1.js");
 noisyLoad("mc.js");
 
+function DrawMcRepo(repo) {
+    var children = {};
+    var branches = {};
+    function recordChild(parentId, childId) {
+	if (parentId) {
+	    if (!(parentId in children)) children[parentId] = {};
+	    children[parentId][childId] = 1;
+	}
+    }
+    function recordBranch(blobId, branchName) {
+	if (!(blobId in branches)) branches[blobId] = {};
+	branches[blobId][branchName] = 1;
+    }
+    function traceBranch(branchName, blobId) {
+	if (blobId) {
+	    var entry = repo.blobs[Mc.Util.blobIdKey(blobId)];
+	    traceBranch(branchName, entry.directParent);
+	    traceBranch(branchName, entry.additionalParent);
+	    recordBranch(blobId, branchName);
+	    recordChild(entry.directParent, blobId);
+	    recordChild(entry.additionalParent, blobId);
+	}
+    }
+    var allBranches = repo.allBranches();
+    for (var tag in allBranches) {
+	traceBranch(repo.prettyTag(tag), allBranches[tag]);
+    }
+
+    var repoWrapper = {
+	childlessRevisions: function() {
+	    var result = {};
+	    var bs = allBranches;
+	    for (var tag in bs) {
+		result[bs[tag]] = 1;
+	    }
+	    return Mc.Util.dict_to_set_list(result);
+	},
+	lookupParents: function(blobId) {
+	    return repo.lookupParents(blobId);
+	},
+	lookupChildren: function(blobId) {
+	    return Mc.Util.dict_to_set_list(children[blobId]);
+	},
+	lookupRev: function(blobId) {
+	    return {
+		branch: Mc.Util.dict_to_set_list(branches[blobId]).join(","),
+		metadata: repo.lookup(blobId).metadata
+	    };
+	}
+    };
+    print(JSON.stringify({children: children, branches: branches}, null, 2));
+    return DrawDvcs.simpleRenderRepository(repoWrapper);
+}
+
 Mc.Tests = {
     pp: function(x) {
         return JSON.stringify(x, null, 2);
@@ -67,11 +121,15 @@ Mc.Tests = {
         Mc.Tests.revisionC = fs.commit({summary: "Fourth commit"});
         d("post-rC");
 
+	print(DrawMcRepo(repo));
+
         fs.merge("BBB");
         d("post-merge");
 
         var rMerger = fs.commit({summary: "Merge BBB into master"});
         d("post-merge-commit");
+
+	print(DrawMcRepo(repo));
 
         fs = new Mc.Checkout(repo, rMerger);
         d("post-rMerger");
@@ -90,6 +148,8 @@ Mc.Tests = {
         var rMerger2 = fs.commit({summary: "Merge master into BBB"});
         d("post-rMerger2-commit");
 
+	print(DrawMcRepo(repo));
+
         print("---------------------------------------------------------------------------");
         Mc.Tests.exportedJson = pp(repo.exportRevisions());
         print(Mc.Tests.exportedJson);
@@ -102,66 +162,20 @@ Mc.Tests = {
         var repo = new Mc.Repository();
         var ed = JSON.parse(Mc.Tests.exportedJson);
         repo.importRevisions(ed);
+	repo.remotes["origin"] = {repoId: ed.repoId};
 
         var fs = new Mc.Checkout(repo);
-	repo.remotes["origin"] = {repoId: ed.repoId};
+// 	fs.writeFile("File B", {"text": ["Conflicting"]});
+// 	fs.commit({summary: "Interfering commit"});
+
         fs.merge("origin/master");
+	print(pp({postMergePreCommit: fs}));
         fs.commit({summary: "Fast-forward to remote's master"});
         print("================================================================= Rt2");
         print(pp({fs: fs}));
         print();
 
-	var children = {};
-	var branches = {};
-	function recordChild(parentId, childId) {
-	    if (parentId) {
-		if (!(parentId in children)) children[parentId] = {};
-		children[parentId][childId] = 1;
-	    }
-	}
-	function recordBranch(blobId, branchName) {
-	    if (!(blobId in branches)) branches[blobId] = {};
-	    branches[blobId][branchName] = 1;
-	}
-	function traceBranch(branchName, blobId) {
-	    if (blobId) {
-		var entry = repo.blobs[Mc.Util.blobIdKey(blobId)];
-		traceBranch(branchName, entry.directParent);
-		traceBranch(branchName, entry.additionalParent);
-		recordBranch(blobId, branchName);
-		recordChild(entry.directParent, blobId);
-		recordChild(entry.additionalParent, blobId);
-	    }
-	}
-	var allBranches = repo.allBranches();
-	for (var tag in allBranches) {
-	    traceBranch(repo.prettyTag(tag), allBranches[tag]);
-	}
-
-	var repoWrapper = {
-	    childlessRevisions: function() {
-		var result = {};
-		var bs = allBranches;
-		for (var tag in bs) {
-		    result[bs[tag]] = 1;
-		}
-		return Mc.Util.dict_to_set_list(result);
-	    },
-	    lookupParents: function(blobId) {
-		return repo.lookupParents(blobId);
-	    },
-	    lookupChildren: function(blobId) {
-		return Mc.Util.dict_to_set_list(children[blobId]);
-	    },
-	    lookupRev: function(blobId) {
-		return {
-		    branch: Mc.Util.dict_to_set_list(branches[blobId]).join(","),
-		    metadata: repo.lookup(blobId).metadata
-		};
-	    }
-	};
-	print(pp({children: children, branches: branches}));
-	print(DrawDvcs.simpleRenderRepository(repoWrapper));
+	print(DrawMcRepo(repo));
     }
 };
 
