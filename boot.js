72d6cbbaefc8a3dbd4f761c4ea93fbdb71f340d6
Tony Garnock-Jones <tonyg@lshift.net>
2009-12-21 17:55:55 +0000

    Catch exceptions.

--- boot.js
+++ boot.js
@@ -1,31 +1,35 @@
 var __$_module_definitions = null;
 (function () {
-     var mdd, md;
+     try {
+	 var mdd, md;
 
-     function foreachNewInstance(f) {
-	 for (var i = 0; i < __$_new_instances.length; i++) {
-	     f(__$_new_instances[i]);
+	 function foreachNewInstance(f) {
+	     for (var i = 0; i < __$_new_instances.length; i++) {
+		 f(__$_new_instances[i]);
+	     }
 	 }
+
+	 foreachNewInstance(function (ni) {
+				if (ni.name === 'net.lshift.synchrotron.module') {
+				    var v = eval('(function () {' + ni.bodyText +
+						 '; return [ModuleDefinitionDirectory,' +
+						 'ModuleDefinition];})()');
+				    mdd = v[0];
+				    md = v[1];
+				}
+			    });
+
+	 var defs = new mdd();
+	 foreachNewInstance(function (ni) {
+				if (ni.objectType === 'moduleDefinition') {
+				    var modDef = md.fromJsonObject(ni);
+				    defs.registerModuleDefinition(modDef);
+				}
+			    });
+
+	 __$_module_definitions = defs;
+	 defs.instantiateModule(__$_goal);
+     } catch (e) {
+	 alert(uneval(e));
      }
-
-     foreachNewInstance(function (ni) {
-			    if (ni.name === 'net.lshift.synchrotron.module') {
-				var v = eval('(function () {' + ni.bodyText +
-					     '; return [ModuleDefinitionDirectory,' +
-					     'ModuleDefinition];})()');
-				mdd = v[0];
-				md = v[1];
-			    }
-			});
-
-     var defs = new mdd();
-     foreachNewInstance(function (ni) {
-			    if (ni.objectType === 'moduleDefinition') {
-				var modDef = md.fromJsonObject(ni);
-				defs.registerModuleDefinition(modDef);
-			    }
-			});
-
-     __$_module_definitions = defs;
-     defs.instantiateModule(__$_goal);
  })();
