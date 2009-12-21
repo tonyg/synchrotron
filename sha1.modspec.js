f175c8bca620f3572ea232b3322b456916d36720
Tony Garnock-Jones <tonyg@lshift.net>
2009-12-21 17:19:03 +0000

    More

---
+++ sha1.modspec.js
@@ -0,0 +1,18 @@
+{
+    "name": "uk.org.pajhome.SHA1",
+    "imports": [],
+    "exports": [{"get_hexcase": "SHA1.get_hexcase",
+		 "set_hexcase": "SHA1.set_hexcase",
+		 "get_b64pad": "SHA1.get_b64pad",
+		 "set_b64pad": "SHA1.set_b64pad",
+		 "get_chrsz": "SHA1.get_chrsz",
+		 "set_chrsz": "SHA1.set_chrsz",
+		 "hex_sha1": "SHA1.hex_sha1",
+		 "b64_sha1": "SHA1.b64_sha1",
+		 "str_sha1": "SHA1.str_sha1",
+		 "hex_hmac_sha1": "SHA1.hex_hmac_sha1",
+		 "b64_hmac_sha1": "SHA1.b64_hmac_sha1",
+		 "str_hmac_sha1": "SHA1.str_hmac_sha1",
+		 "encode_utf8": "SHA1.encode_utf8",
+		 "decode_utf8": "SHA1.decode_utf8"}]
+}
